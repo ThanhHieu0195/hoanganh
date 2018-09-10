925556879bcc316e29abcd6f1723ae9fae6939c3
hieutct <hieutct@solazu.com>
2018-09-11 00:47:03 +0700

    add page home 2

--- includes/templates/home.php
+++ includes/templates/home.php
@@ -1,11 +1,8 @@
 <?php
 get_header('bosevent');
 $path_template_url = get_template_directory_uri();
-$page = get_page_by_title('home');
-$pageID = get_option('page_on_front');
-if (!empty($pageID)) {
-    $page = get_post($pageID);
-}
+global $post;
+$page = $post;
 ?>
 <?php
 // render all file folder home
