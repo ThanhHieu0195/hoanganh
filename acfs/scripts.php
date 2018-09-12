70d6dc703308942f15bdbd23626a3489e711608e
hieutct <hieutct@solazu.com>
2018-09-12 09:55:12 +0700

    add home 2

--- acfs/scripts.php
+++ acfs/scripts.php
@@ -813,7 +813,7 @@ if( function_exists('acf_add_local_field_group') ):
                 array (
                     'param' => 'page',
                     'operator' => '==',
-                    'value' => '67',
+                    'value' => '109',
                 ),
             ),
         ),
