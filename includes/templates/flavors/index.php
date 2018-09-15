c25c3a9bf77ba2026a9c68f3c971e8dc20d07006
hieutct <hieutct@solazu.com>
2018-09-15 23:55:09 +0700

    add code

---
+++ includes/templates/flavors/index.php
@@ -0,0 +1,176 @@
+<div class="block-content">
+    <div class="sc-about-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 col-equal">
+                    <div class="sc-main-title sc-main-title--big">Về Hoàng Anh</div>
+                </div>
+                <div class="col-md-7 col-equal">
+                    <div class="sc-quote">
+                        <div class="sc-quote__icon"></div>
+                        <div class="sc-quote__content">
+                            <div class="sc-quote__content__title">CHÚNG TÔI LÀ HOÀNG ANH</div>
+                            <div class="sc-quote__content__des">
+                                Với sự nhiệt huyết và niềm đam mê, chúng tôi hiểu làm thế nào để
+                                truyền cảm hứng cho mọi người tại Hoàng Anh, tạo môi trường làm
+                                việc chuyên nghiệp, và dẫn dắt công việc lên một tầm cao mới.
+                            </div>
+                        </div>
+                        <div class="sc-quote__bottom">
+                            <div class="sc-quote__bottom__author">DUY T. PHAM</div>
+                            <div class="sc-quote__bottom__position">General Director</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <nav class="breadcrumb-wapper">
+            <ul class="breadcrumb">
+                <li class="breadcrumb__item"><a class="breadcrumb__link" href="/index.html">home</a></li><i class="fas fa-angle-right"></i>
+                <li class="breadcrumb__item active"><a class="breadcrumb__link active" href="#">Flavor</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="sc-product-layout-1">
+        <div class="container">
+            <div class="sc-main-title text-center">Các Dòng Sản Phẩm Hương Cho Ngành Thực Phẩm </div>
+            <div class="block-item left">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-4 item">
+                        <div class="block-image"><a href="#"><img src="<?= $path_template_url ?>/assets/images/flavor/flavor1.png"></a></div>
+                    </div>
+                    <div class="col-lg-offset-4 col-md-offset-0 col-xs-12 col-sm-12 col-md-8">
+                        <div class="block-content">
+                            <div class="title">Nhóm Hương Ngọt</div>
+                            <div class="description">Mùi và hương vị là cái gì đó rất gần gũi nhưng cũng lại mơ hồ, khó nắm bắt. Mùi vị có một ảnh hưởng mạnh mẽ đến đời sống cảm xúc của con người, nó đánh thức những ký ức đồng thời tạo ra nhiều cảm xúc trong cuộc sống hàng ngày.
+                                <ul>
+                                    <li>Mùi và hương vị là cái gì đó rất gần gũi nhưng cũng lại mơ hồ.</li>
+                                    <li>Mùi vị có một ảnh hưởng mạnh mẽ đến đời sống cảm xúc của con người.</li>
+                                    <li>Đánh thức những ký ức đồng thời tạo ra nhiều cảm xúc trong cuộc sống hàng ngày.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="block-item right">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-4 item">
+                        <div class="block-image"><a href="#"><img src="<?= $path_template_url ?>/assets/images/flavor/flavor2.png"></a></div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-8">
+                        <div class="block-content">
+                            <div class="title">Nhóm Hương Ngọt</div>
+                            <div class="description">Mùi và hương vị là cái gì đó rất gần gũi nhưng cũng lại mơ hồ, khó nắm bắt. Mùi vị có một ảnh hưởng mạnh mẽ đến đời sống cảm xúc của con người, nó đánh thức những ký ức đồng thời tạo ra nhiều cảm xúc trong cuộc sống hàng ngày.
+                                <ul>
+                                    <li>Mùi và hương vị là cái gì đó rất gần gũi nhưng cũng lại mơ hồ.</li>
+                                    <li>Mùi vị có một ảnh hưởng mạnh mẽ đến đời sống cảm xúc của con người.</li>
+                                    <li>Đánh thức những ký ức đồng thời tạo ra nhiều cảm xúc trong cuộc sống hàng ngày.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sc-technology-layout-1">
+        <div class="container">
+            <div class="row">
+                <div class="sc-main-title text-center">Công nghệ</div>
+                <div class="sc-subtitle">Mùi và hương vị là cái gì đó rất gần gũi nhưng cũng lại mơ hồ, khó nắm bắt. Mùi vị có một ảnh hưởng mạnh mẽ đến đời sống cảm xúc của con người</div>
+                <div class="col-xs-12 col-sm-12 col-md-4">
+                    <div class="block-item">
+                        <div class="block-image"><img src="<?= $path_template_url ?>/assets/images/flavor/congnghe1.png"></div>
+                        <div class="block-content">
+                            <div class="title">Hương Tổng Hợp</div>
+                            <div class="description">Mùi và Hương Vị là cái gì đó rất gần gũi nhưng cũng lại mơ hồ, khó nắm bắt. Lại đầy sự kích thích.</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-4">
+                    <div class="block-item">
+                        <div class="block-image"><img src="<?= $path_template_url ?>/assets/images/flavor/congnghe2.png"></div>
+                        <div class="block-content">
+                            <div class="title">Hương Phản Ứng Nhiệt</div>
+                            <div class="description">Mùi và Hương Vị là cái gì đó rất gần gũi nhưng cũng lại mơ hồ, khó nắm bắt. Lại đầy sự kích thích khứu giác.</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-4">
+                    <div class="block-item">
+                        <div class="block-image"><img src="<?= $path_template_url ?>/assets/images/flavor/congnghe3.png"></div>
+                        <div class="block-content">
+                            <div class="title">Hương Vị Bao</div>
+                            <div class="description">Mùi và Hương Vị là cái gì đó rất gần gũi nhưng cũng lại mơ hồ, khó nắm bắt. Lại đầy sự kích thích khứu giác.</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-6">
+                    <div class="block-item">
+                        <div class="block-image"><img src="<?= $path_template_url ?>/assets/images/flavor/congnghe4.png"></div>
+                        <div class="block-content">
+                            <div class="title">Hương Tự Nhiên</div>
+                            <div class="description">Mùi và Hương Vị là cái gì đó rất gần gũi nhưng cũng lại mơ hồ, khó nắm bắt. Lại đầy sự kích thích khứu giác.</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-6">
+                    <div class="block-item">
+                        <div class="block-image"><img src="<?= $path_template_url ?>/assets/images/flavor/congnghe1.png"></div>
+                        <div class="block-content">
+                            <div class="title">Hương ..........</div>
+                            <div class="description">Mùi và Hương Vị là cái gì đó rất gần gũi nhưng cũng lại mơ hồ, khó nắm bắt. Lại đầy sự kích thích khứu giác.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sc-product-layout-2">
+        <div class="container">
+            <div class="row">
+                <div class="sc-main-title text-center">Các Dạng Hương</div>
+                <div class="sc-subtitle">Mùi và hương vị là cái gì đó rất gần gũi nhưng cũng lại mơ hồ, khó nắm bắt. Mùi vị có một ảnh hưởng mạnh mẽ đến đời sống cảm xúc của con người</div>
+                <div class="col-xs-12 col-sm-12 col-md-6">
+                    <div class="block-item">
+                        <div class="block-image"><img src="<?= $path_template_url ?>/assets/images/flavor/cacdanghuong1.png"></div>
+                        <div class="block-content">
+                            <div class="title">Hương Dạng Lỏng</div>
+                            <div class="description">Mùi và Hương Vị là cái gì đó rất gần gũi nhưng cũng lại mơ hồ, khó nắm bắt. Lại đầy sự kích thích khứu giác.</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-6">
+                    <div class="block-item">
+                        <div class="block-image"><img src="<?= $path_template_url ?>/assets/images/flavor/cacdanghuong2.png"></div>
+                        <div class="block-content">
+                            <div class="title">Hương Dạng Bột</div>
+                            <div class="description">Mùi và Hương Vị là cái gì đó rất gần gũi nhưng cũng lại mơ hồ, khó nắm bắt. Lại đầy sự kích thích.</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-6">
+                    <div class="block-item">
+                        <div class="block-image"><img src="<?= $path_template_url ?>/assets/images/flavor/cacdanghuong3.png"></div>
+                        <div class="block-content">
+                            <div class="title">Hương Dạng Paste</div>
+                            <div class="description">Mùi và Hương Vị là cái gì đó rất gần gũi nhưng cũng lại mơ hồ, khó nắm bắt. Lại đầy sự kích thích.</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-6">
+                    <div class="block-item">
+                        <div class="block-image"><img src="<?= $path_template_url ?>/assets/images/flavor/cacdanghuong4.png"></div>
+                        <div class="block-content">
+                            <div class="title">Hương Dạng Đậm Đặc</div>
+                            <div class="description">Mùi và Hương Vị là cái gì đó rất gần gũi nhưng cũng lại mơ hồ, khó nắm bắt. Lại đầy sự kích thích khứu giác.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ includes/templates/research/index.php
@@ -0,0 +1,104 @@
+<div class="block-content">
+    <div class="sc-about-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 col-equal">
+                    <div class="sc-main-title sc-main-title--big">Về Hoàng Anh</div>
+                </div>
+                <div class="col-md-7 col-equal">
+                    <div class="sc-quote">
+                        <div class="sc-quote__icon"></div>
+                        <div class="sc-quote__content">
+                            <div class="sc-quote__content__title">CHÚNG TÔI LÀ HOÀNG ANH</div>
+                            <div class="sc-quote__content__des">
+                                Với sự nhiệt huyết và niềm đam mê, chúng tôi hiểu làm thế nào để
+                                truyền cảm hứng cho mọi người tại Hoàng Anh, tạo môi trường làm
+                                việc chuyên nghiệp, và dẫn dắt công việc lên một tầm cao mới.
+                            </div>
+                        </div>
+                        <div class="sc-quote__bottom">
+                            <div class="sc-quote__bottom__author">DUY T. PHAM</div>
+                            <div class="sc-quote__bottom__position">General Director</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <nav class="breadcrumb-wapper">
+            <ul class="breadcrumb">
+                <li class="breadcrumb__item"><a class="breadcrumb__link" href="/index.html">home</a></li><i class="fas fa-angle-right"></i>
+                <li class="breadcrumb__item active"><a class="breadcrumb__link active" href="#">Nghiên cứu</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="block-section">
+        <div class="container">
+            <div class="sc-team-block">
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-1">
+                        <div class="sc-team-block__block-image"><img src="<?= $path_template_url ?>/assets/images/home/xu_huong.png" alt=""></div>
+                    </div>
+                    <div class="col-md-6 col-md-offset-1">
+                        <div class="sc-team-block__block-content">
+                            <div class="sc-main-title sc-team-block__title">Xu Hướng</div>
+                            <div class="sc-team-block__description">Khởi nghiệp là một nhà phân phối nguyên liệu thực phẩm, Công ty Hương liệu và Nguyên liệu thực phẩm HOÀNG ANH đã mạnh dạn đầu tư nghiên cứu phát triển để trở thành nhà tạo hương thực phẩm Việt Nam đầu tiên. Và mới đây, ngày 27/01/2017 Hoàng Anh đã ký kết hợp tác với Viện Dinh dưỡng Quốc gia để nghiên cứu về nguyên liệu dinh dưỡng trong thực phẩm, đánh dấu một bước tiến mới trên con đường phát triển của công ty.<br>
+                                Sở dĩ Hoàng Anh có bước chuyển mình lần thứ 2 này vì với xu thế mới trên thế giới và tại Việt Nam hiện nay, người tiêu dùng ngày càng tiếp cận nhiều thông tin hơn về dinh dưỡng. Họ không chỉ dừng mối quan tâm lại ở việc ăn có ngon hay không, có bổ dưỡng hay không, mà còn quan tâm ngày càng nhiều hơn nữa ...
+                            </div>
+                            <div class="sc-button sc-team-block__btn"><a class="main-color" href="#"><span>Xem Thêm</span></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sc-innovation">
+        <div class="block-section-lg">
+            <div class="container">
+                <div class="sc-main-title">Product Innovation</div>
+                <div class="col-md-6">
+                    <div class="sc-innovation__item" style="background-image:url(./assets/images/research/product_innovation1.png);">
+                        <div class="sc-innovation__title">Research and Development</div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="sc-innovation__item" style="background-image:url(./assets/images/research/product_innovation2.png);">
+                        <div class="sc-innovation__title">Culinary</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sc-innovation--rd-center">
+        <div class="block-section-lg">
+            <div class="container">
+                <div class="sc-main-title">R&D Center</div>
+                <div class="col-md-4">
+                    <div class="sc-innovation__wrapper-item">
+                        <div class="sc-innovation__item" style="background-image:url(./assets/images/research/r&amp;d1.png);">
+                            <div class="sc-innovation__title">Research and Development</div>
+                        </div>
+                        <div class="sc-button sc-innovation__button"><a class="main-color" href="#"><span>Xem Thêm</span></a></div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="sc-innovation__wrapper-item">
+                        <div class="sc-innovation__item" style="background-image:url(./assets/images/research/r&amp;d2.png);">
+                            <div class="sc-innovation__title">Culinary</div>
+                        </div>
+                        <div class="sc-button sc-innovation__button"><a class="main-color" href="#"><span>Xem Thêm</span></a></div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="sc-innovation__wrapper-item">
+                        <div class="sc-innovation__item" style="background-image:url(./assets/images/research/r&amp;d3.png);">
+                            <div class="sc-innovation__title">Culinary</div>
+                        </div>
+                        <div class="sc-button sc-innovation__button"><a class="main-color" href="#"><span>Xem Thêm</span></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
