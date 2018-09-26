95d80d3aa2ceeee2ef4318fb05da11930e35e987
tailm <tailm@solazu.com>
2018-09-26 23:32:28 +0700

    update form

--- comments.php
+++ comments.php
@@ -49,6 +49,7 @@ if ( post_password_required() ) {
 	endif; // Check for have_comments().
 	?>
 	<h3 id="reply-title" class="comment-reply-title">Leave a Reply <small><a rel="nofollow" id="cancel-comment-reply-link" href="#" style="display:none;">Cancel reply</a></small></h3>
+	<label for="comment">Comment</label>
 	<div class="sc-form__group">
 		<div class="sc-form__item--half">
 			<input class="sc-form__control" type="text" name="name" placeholder="Your name">
