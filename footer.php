c321db3528a2f9f8cd82782f96ca465eaaf2c64d
tailm <tailm@solazu.com>
2018-09-27 20:50:43 +0700

    update change css home page

--- footer.php
+++ footer.php
@@ -25,7 +25,8 @@ $videos = get_field('footer-videos', 'option', []);
         <div class="container">
           <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-12">
-                <?= get_field('footer-text-banner', 'option', '') ?>
+                <div class="title">Subscribe for <span>Hoang Anh</span> viewpoints!</div>
+                <div class="description"><?= get_field('footer-text-banner', 'option', '') ?></div>
             </div>
             <div class="col-lg-6 col-md-6 col-sm-12">
               <div class="form">
