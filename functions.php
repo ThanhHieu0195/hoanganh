2c00a98f7f406c734f0267d36eda388d4b7e4c29
hieutct <hieutct@solazu.com>
2018-09-27 23:27:35 +0700

    add code

--- functions.php
+++ functions.php
@@ -179,7 +179,7 @@ function get_breadcrumb() {
                     $cat = get_the_category();
                     if (!empty($cat)) {
                         echo "<i class=\"fas fa-angle-right\"></i>";
-                        echo $cat . ' &bull; ';
+                        echo $cat[0]->name . ' &bull; ';
                     }
 
                     if (is_single()) {
