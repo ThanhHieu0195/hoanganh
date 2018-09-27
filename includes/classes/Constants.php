2bf3a7a225cf7fa20060020745d637eeee1e8161
hieutct <hieutct@solazu.com>
2018-09-27 23:05:49 +0700

    add template

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -19,7 +19,7 @@ Class Constants {
         self::DEFAULT_SLUG  . 'recruitment/' => 'recruitment',
         self::DEFAULT_SLUG  . 'contact/' => 'contact',
         self::DEFAULT_SLUG  . 'post-listing/' => 'post-listing',
-        self::DEFAULT_SLUG  . 'technology-listing/' => 'technology-listing',
+//        self::DEFAULT_SLUG  . 'technology-listing/' => 'technology-listing',
 
         // self::DEFAULT_SLUG . 'en/' => 'home',
     ];
