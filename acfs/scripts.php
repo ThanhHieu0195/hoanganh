33cb9d85672f1682b7f941d3786294dad70b0565
hieutct <thanhhieu0195@gmail.com>
2018-11-28 12:00:52 -0500

    fix bugs

--- acfs/scripts.php
+++ acfs/scripts.php
@@ -18,5 +18,51 @@ if( function_exists('acf_add_local_field_group') ):
     foreach (glob($dir_path . "/options_page/*.php") as $filename) {
         include $filename;
     }
-
+    
+     acf_add_local_field_group(array (
+        'key' => 'company_news',
+        'title' => 'Company News',
+        'fields' => [
+            array (
+            'key' => 'field_company_new',
+            'label' => 'Company New',
+            'name' => 'company_new',
+            'type' => 'checkbox',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array (
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'choices' => array (
+                '1' => 'Show in company news'
+            ),
+            'allow_custom' => 0,
+            'save_custom' => 0,
+            'default_value' => array (
+            ),
+            'layout' => 'vertical',
+            'toggle' => 0,
+            'return_format' => 'value',
+        ),
+        ],
+        'location' => array (
+            array (
+               array (
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'post',
+            ),
+            )
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'active' => 1,
+        'description' => '',
+    ));
 endif;
--- includes/templates/vi/about-us/4-news.php
+++ includes/templates/vi/about-us/4-news.php
@@ -2,4 +2,4 @@
  $title = get_field('news-title', $page->ID, '');
  $number = get_field('news-number', $page->ID, 3);
  ?>
- <?= do_shortcode('[post-carousel title="' . $title . '" number="' . $number . '" /]') ?>
+ <?= do_shortcode('[post-carousel type="company_news" title="' . $title . '" number="' . $number . '" /]') ?>
