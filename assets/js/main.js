acc5e0381bce37995756d3e44904e4f320671078
tailm <tailm@solazu.com>
2018-12-13 01:07:01 +0700

    bug#14

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,24 +56,24 @@ $(document).ready(function() {
 		fixedContentPos: false
 	});
 });
-var map;
-function loadMarkers() {
-  console.log('creating markers')
-  map.data.forEach(function(feature) {
+// var map;
+// function loadMarkers() {
+//   console.log('creating markers')
+//   map.data.forEach(function(feature) {
     
-    // geojson format is [longitude, latitude] but google maps marker position attribute
-    // expects [latitude, longitude]
-    var latitude = feature.getGeometry().get().lat()
-    var longitude = feature.getGeometry().get().lng()
-    var titleText = feature.getProperty('title')
+//     // geojson format is [longitude, latitude] but google maps marker position attribute
+//     // expects [latitude, longitude]
+//     var latitude = feature.getGeometry().get().lat()
+//     var longitude = feature.getGeometry().get().lng()
+//     var titleText = feature.getProperty('title')
 
-    var marker = new google.maps.Marker({
-      position: {lat: latitude, lng:longitude},
-      title: titleText,
-      map: map
-     });
-  });
-}
+//     var marker = new google.maps.Marker({
+//       position: {lat: latitude, lng:longitude},
+//       title: titleText,
+//       map: map
+//      });
+//   });
+// }
 function myMap() {
   let lat = parseFloat($('#googleMap').data('lat'));
   let lng = parseFloat($('#googleMap').data('lng'));
