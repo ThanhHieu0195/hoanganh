759246e0c1de9bad6f288f3e8a0523c43219261d
tailm <tailm@solazu.com>
2018-12-17 09:19:25 +0700

    bug#22

--- includes/shortcodes/templates/flavor-technology/flavor-technology-1.php
+++ includes/shortcodes/templates/flavor-technology/flavor-technology-1.php
@@ -8,6 +8,6 @@ extract($params);
         <div class="description">
             <?= $description ?>
         </div>
-        <div class="read-more"><?= translate_i18n('Xem Thêm') ?></div>
+        <!-- <div class="read-more"><?= translate_i18n('Xem Thêm') ?></div> -->
     </div>
 </div>
--- includes/shortcodes/templates/ingredient/ingredient-1.php
+++ includes/shortcodes/templates/ingredient/ingredient-1.php
@@ -10,6 +10,6 @@ extract($params);
         <div class="description">
             <?= $description ?>
         </div>
-        <div class="read-more"><?= translate_i18n('Xem Thêm') ?></div>
+        <!-- <div class="read-more"><?= translate_i18n('Xem Thêm') ?></div> -->
     </div>
 </div>
